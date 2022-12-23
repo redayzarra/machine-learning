30f501500f3e0c6f1dfc26b5d9f27ca87ce5a448
ReDay Zarra <redayzarra@gmail.com>
2022-12-22 22:07:02 -0500

    Importing necessary libraries for data preprocessing

---
+++ Preprocessing/preprocessing_tools.py
@@ -0,0 +1,13 @@
+"""
+Data Pre-Processing is the first stage of the machine learning process. Here, we
+will:
+  1. Import the data and all necessary libraries and modules
+  2. Clean the data
+  3. Split the data into training and test sets
+
+Importing the necessary libraries and modules we need to start preprocessing 
+our data. 
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
