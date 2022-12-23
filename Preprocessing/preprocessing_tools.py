32d0eafd8c5bf44d7c66bab0ca0e3fd268b8981a
ReDay Zarra <redayzarra@gmail.com>
2022-12-22 23:04:43 -0500

    Importing the dataset and declaring the matrix of features and dependent variable

--- Preprocessing/preprocessing_tools.py
+++ Preprocessing/preprocessing_tools.py
@@ -11,3 +11,19 @@ our data.
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
+"""
+Importing the dataset requires us to use the pandas library which will import
+the dataset in a new variable. Then we have to create the matrix of features and
+then the dependent variable vector. The features are the columns with which you
+will predict the final decision (dependent variable). So, the dependent variable
+is really what you WANT to predict.
+"""
+dataset = pd.read_csv('Preprocessing/Data.csv') # Pandas reads the dataset and creates a data frame. The data frame is stored in the variable dataset.
+#   dataset.iloc[rows, columns]
+x = dataset.iloc[: , :-1].values # Matrix of Features - Gets values from ALL ROWS, and all columns EXCEPT FOR LAST using Pandas' iloc function, which means locate indices, to single out the rows with our data and columns of features
+y = dataset.iloc[: , -1].values # Dependent Variable - Gets values from ALL ROWS, and ONLY LAST COLUMN using Pandas' iloc function
+
+print(x)
+print(y)
