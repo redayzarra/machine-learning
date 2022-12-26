1d0c7abb38661bc97069ed8fe8f12682f1cc967c
ReDay Zarra <redayzarra@gmail.com>
2022-12-25 21:59:02 -0500

    Examining the divided data sets and testing with different random state seeds

--- Preprocessing/preprocessing_tools.py
+++ Preprocessing/preprocessing_tools.py
@@ -95,3 +95,8 @@ and testing sets.
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 1)
+
+print(X_train)
+print(X_test)
+print(Y_train)
+print(Y_test)
