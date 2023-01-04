fc6974ff69476379efde5af49f955bab3b37bb3a
ReDay Zarra <redayzarra@gmail.com>
2023-01-04 14:38:45 -0500

    Adding notes on the five main methods for building multiple linear regression models

--- Multi-Linear-Regression/ML_Regression.py
+++ Multi-Linear-Regression/ML_Regression.py
@@ -13,6 +13,9 @@ of these would be individual features that would affect the overall growth of ou
 potatoes and we would set each one as a bn*xn component in our equation. So our 
 equation would be: Potatoes (ŷ) = 8 tons (b0)  + 3 tons Fertilizer + 2 tons H2O etc.
 
+
+THE FIVE ASSUMPTIONS FOR LINEAR REGRESSION:
+
 However, you cannot just blindly apply linear regression. You have to make sure 
 that your dataset is fit for using a linear regression model. There are five main
 assumptions to make before using linear regression:
@@ -46,6 +49,9 @@ While this is not one of the five assumptions, an extra check we can run when us
 linear regression is an outlier check. An outlier check would find outliers in a 
 dataset and remove them from significantly affecting the linear regression model.
 
+
+DUMMY VARIABLES AND THEIR TRAP:
+
 Dummy variables are used when we encounter categorical data in our dataset. For
 example, if we are looking at the profit a company generates from spending in 
 marketing, research, customer service.. we can just use the numerical values for
@@ -67,4 +73,28 @@ the linear regression model to include the coefficient for CA in the y-intercept
 When you are building a model, it is crucial to always remove one dummy variable 
 because it causes multicollinearity since it creates two variables that affect 
 each other in some way. If you have 9 dummy variables, you should only include 8. 
+
+
+BUILDING A MULTI-LINEAR REGRESSION MODEL:
+
+You may encounter data with many independent variables, and one of the first steps
+for building a linear regression model is to decide which independent variables to
+keep and which to throw out. It is necessary to discard some of your independent 
+variables (features) because the model might suffer from having these extra 
+variables to account for. 
+
+There are five main methods to building multiple linear regression models:
+
+  1. All In - these are cases where you would just blindly throw in all of your
+  variables. You would do this if you have prior knowledge that these are variables
+  you should be using. You would also be using this for preparing for backward
+  elimination.
+
+  2. Backward Elimination
+
+  3. Forward Selection
+
+  4. Bidirectional Elimination
+
+  5. Score Comparison
 """
